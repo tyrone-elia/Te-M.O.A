43b312191eb8ed99b916355c7cda69339ba7c802
Tyrone Elia <tyroneelia@gmail.com>
2022-09-14 12:15:08 +1200

    temoa start fullstack

---
+++ client/components/AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+function aboutUs () {
+
+return (
+<div>
+    <p>
+      Te M.O.A was created through passion to promote and showcase our beautiful country and people.
+    </p>
+    <p>
+     Based at The Queen Charlotte Tavern
+    </p>
+    <p className="img" >
+      <img src='/images/TeMoa3.png' alt='pic'/>
+    </p>
+</div>
+)
+}
+
+export default aboutUs
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
+import AboutUs from './AboutUs'
 import { fetchFruits } from '../actions'
 
 function App() {
@@ -13,12 +14,16 @@ function App() {
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
+        <h1>Te Mauri Ora Adventures</h1>
+        
         <ul>
           {fruits.map((fruit) => (
             <li key={fruit}>{fruit}</li>
           ))}
         </ul>
+        <div>
+        <AboutUs/>
+        </div>
       </div>
     </>
   )
